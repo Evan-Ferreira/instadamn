3a1d6aca16b22b37d9ac64b0ac861c20eb31acb4
Evan Ferreira <evanjfer@gmail.com>
2025-09-26 15:48:02 -0400

    add default user routes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,23 @@
 import express from 'express';
+import { requestLogger } from './middleware/request-logger';
+import cors from 'cors';
+import { readJsonFile } from './utils/json';
 
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello, TypeScript with Express!');
+app.use(cors());
+app.use(requestLogger);
+
+app.get('/users', (req, res) => {
+    res.send(readJsonFile('./src/db/users.json'));
+});
+
+app.get('/users/:username', (req, res) => {
+    const { username } = req.params;
+    const users = readJsonFile('./src/db/users.json');
+    const posts = users[username];
+    res.send(posts);
 });
 
 app.listen(port, () => {
